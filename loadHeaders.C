8d64452bf13baea6617050322aa4f59b0d37466b
Kaya Tatar <tatark@mit.edu>
2015-05-20 17:10:06 -0400

    un-named script that loads files in a ROOT session.

---
+++ loadHeaders.C
@@ -0,0 +1,6 @@
+// in a ROOT session type ".x /net/hisrv0001/home/tatar/code/HIUtils/loadHeaders.C" to load all the header files listed below.
+{
+  gROOT->ProcessLine(".L /net/hisrv0001/home/tatar/code/HIUtils/histoUtil.h");
+  gROOT->ProcessLine(".L /net/hisrv0001/home/tatar/code/HIUtils/systemUtils.h");
+  gROOT->ProcessLine(".L /net/hisrv0001/home/tatar/code/HIUtils/smallPhotonUtil.h");
+}
