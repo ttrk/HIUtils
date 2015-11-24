d7ec5c25a1c0f6b7cb75ced41cf4a2a0b2ad603e
Kaya Tatar <tatark@mit.edu>
2015-11-24 01:38:44 +0100

    macro to quickly draw objects from a TTree*

---
+++ quickMacros/treeDraw.C
@@ -0,0 +1,80 @@
+/*
+ * treeDraw.C
+ *
+ * macro to quickly draw objects from a TTree*
+ */
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TCut.h>
+#include <TH1.h>
+#include <TH1D.h>
+#include <TCanvas.h>
+
+#include <string>
+#include <iostream>
+
+void treeDraw();
+
+void treeDraw()
+{
+    ///// enter input /////
+    const char* inputFilePath = "HiForest.root";
+    const char* treePath = "ggHiNtuplizer/EventTree";
+    const int numFriends = 3;
+    const char* treeFriendList[numFriends] = {"akPu3PFJetAnalyzer/t", "hltanalysis/HltTree", "skimanalysis/HltTree"};
+
+    std::string formula = "phoEt";
+    TCut selection = "abs(phoEta) < 1.44";
+
+    // input for TH1D
+    std::string hName  = "h1";
+    std::string hTitle = "";
+    int nBins   = 100;
+    double xLow = 0;
+    double xUp  = 100;
+
+    // input for TCanvas
+    std::string cnvName  = "cnv";
+    std::string cnvTitle = "";
+    ///// enter input - END /////
+
+    TFile* inputFile = new TFile(inputFilePath,"READ");
+    TTree* tree = (TTree*)inputFile->Get(treePath);
+    TTree* treeFriends[numFriends];
+    for (int i=0; i<numFriends; ++i)
+    {
+        treeFriends[i] = (TTree*)inputFile->Get(treeFriendList[i]);
+        tree->AddFriend(treeFriends[i],Form("t%d",i));
+        std::cout<<"added as friend : "<<treeFriendList[i]<<std::endl;
+    }
+
+    // initialize histograms, graphs etc.
+    // NOTE : this step must be done after the input file is opened.
+    TH1::SetDefaultSumw2();
+
+    TH1D* h = new TH1D(hName.c_str() , hTitle.c_str() ,nBins ,xLow ,xUp);
+    h->SetMarkerStyle(kFullCircle);
+    h->SetMarkerColor(kBlack);
+
+    TCanvas* c = new TCanvas(cnvName.c_str() , cnvTitle.c_str() ,600 ,600 );
+    // drawing
+    c->cd();
+    tree->Draw(Form("%s>>%s", formula.c_str(), h->GetName()),selection.GetTitle(),"goff");
+    h->Draw("e same");
+
+    c->SaveAs(Form("cnv_%s.png", h->GetName()));
+    c->SaveAs(Form("cnv_%s.pdf", h->GetName()));
+    c->SaveAs(Form("cnv_%s.C", h->GetName()));
+    c->SaveAs(Form("cnv_%s.root", h->GetName()));
+    c->Close();
+
+    inputFile->Close();
+}
+
+int main()
+{
+    treeDraw();
+
+    return 0;
+}
